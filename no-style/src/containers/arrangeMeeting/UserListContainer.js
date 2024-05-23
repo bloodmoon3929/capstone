002a6f0b16ea5757f5bbfa3de0a967a4f5648e3d
Dongubak <91736442+Dongubak@users.noreply.github.com>
2024-05-23 15:19:29 +0900

    dd
    dd

--- no-style/src/containers/arrangeMeeting/UserListContainer.js
+++ no-style/src/containers/arrangeMeeting/UserListContainer.js
@@ -10,16 +10,16 @@ import axios from "axios";
 
 const UserListContainer = ({users}) => {
    const dispatch = useDispatch();
-   const uid = useSelector((state) => state.login.uid);
+
    const add = (e) => { ///e는 현재 유저임
       return () => {
          dispatch(addUser(e.table)); ///e.table은 현재 유저의 테이블을 store.user에 concat하는 부분임
       }
    }
 
-   // useEffect(() => {
+   useEffect(() => {
    //    /// 수정 1 현재 사용자의 강의 정보들을 모두 읽어낸 후 store.user의 user에 저장함
-   //    console.log('initializing current user\'s timetable');
+      console.log('initializing current user\'s timetable');
    //    (async function() {
    //       const docRef = doc(db, 'user', localStorage.getItem("user").uid);
    //       console.log(docRef);
@@ -34,24 +34,25 @@ const UserListContainer = ({users}) => {
    //    }
    //  }, []);
 
-   //    const listdata = async ()=>{
-   //       try
-   //       {
-   //          const response = await axios.post(
-   //             'http://localhost:3001/arrageMeeting',{uid}
-   //          );
-   //          console.log(response.data);
-   //          await dispatch(ArrangeMeetingSchedule(response.data))
-   //       }
-   //       catch(e)
-   //       {
-   //          console.log(e);
-   //       }
+      const listdata = async ()=>{
+         try {
+            const { uid } = JSON.parse(localStorage.getItem("user"));
+            console.log(`current uid : ${uid}`);
+            const response = await axios.post(
+               'http://localhost:3001/api/initUser',{uid}
+            );
 
-   //    }
+            await dispatch(initUser(response.data));
+         }
+         catch(e)
+         {
+            console.log(e);
+         }
 
-   //    listdata();
-   //  }, []);
+      }
+
+      listdata();
+    }, []);
 
    return (
       <UserList users={users} add={add}></UserList>
