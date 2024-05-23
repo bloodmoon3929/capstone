b0dac21f0d953577519379ca46a6e7817fc778d9
Dongubak <91736442+Dongubak@users.noreply.github.com>
2024-05-23 14:55:24 +0900

    ㅇㅇ
    ㅇㅇ

--- no-style/src/containers/arrangeMeeting/UserListContainer.js
+++ no-style/src/containers/arrangeMeeting/UserListContainer.js
@@ -15,6 +15,7 @@ const UserListContainer = ({users}) => {
       }
    }
 
+<<<<<<< Updated upstream
    useEffect(() => {
       /// 수정 1 현재 사용자의 강의 정보들을 모두 읽어낸 후 store.user의 user에 저장함
       console.log('initializing current user\'s timetable');
@@ -23,11 +24,23 @@ const UserListContainer = ({users}) => {
          const docSnap = await getDoc(docRef);
          await dispatch(initUser(docSnap.data().table));
       })();
+=======
+   // useEffect(() => {
+   //    /// 수정 1 현재 사용자의 강의 정보들을 모두 읽어낸 후 store.user의 user에 저장함
+   //    console.log('initializing current user\'s timetable');
+   //    (async function() {
+   //       const docRef = doc(db, 'user', localStorage.getItem("user").uid);
+   //       console.log(docRef);
+   //       const docSnap = await getDoc(docRef);
+   //       console.log(docSnap.data().table);
+   //       await dispatch(initUser(docSnap.data().table));
+   //    })();
+>>>>>>> Stashed changes
 
-      return () => {
-         dispatch(clearUser());
-      }
-    }, []);
+   //    return () => {
+   //       dispatch(clearUser());
+   //    }
+   //  }, []);
 
    return (
       <UserList users={users} add={add}></UserList>
